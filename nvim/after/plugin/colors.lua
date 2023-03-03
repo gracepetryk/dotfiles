8a1039fbc10017ae41ad170643230db769d3f993
Grace Petryk <gpetryk@mysteryscience.com>
2023-03-03 13:40:19 -0700

    update set, colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,4 +1,5 @@
 vim.cmd [[colorscheme vscode]]
 
 vim.api.nvim_set_hl(0, 'ColorColumn', {bg='#292929'})
+vim.api.nvim_set_hl(0, 'Pmenu', {link='Normal'})
 
--- nvim/lua/gpetryk/set.lua
+++ nvim/lua/gpetryk/set.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 vim.opt.errorbells = false
 
 vim.opt.termguicolors = true
@@ -13,16 +15,14 @@ vim.opt.scrolloff = 10
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-
-vim.g.mapleader = " "
-
 vim.opt.laststatus = 3
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.signcolumn = 'yes:2'
+vim.opt.colorcolumn = '81'
 
 vim.opt.shiftwidth = 4
 
-vim.opt.colorcolumn = '81'
+vim.opt.wrap = false
