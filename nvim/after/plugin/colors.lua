5f966220be7753a34381ee4cd457ef5580ab4495
Grace Petryk <gpetryk@mysteryscience.com>
2023-03-08 11:37:54 -0700

    hl

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,5 +2,6 @@ vim.cmd [[colorscheme vscode]]
 
 vim.api.nvim_set_hl(0, 'ColorColumn', {bg='#292929'})
 vim.api.nvim_set_hl(0, 'Pmenu', {link='Normal'})
-vim.api.nvim_set_hl(0, 'TreesitterContext', {bg='#323232'})
+vim.api.nvim_set_hl(0, 'LspSignatureActiveParameter', {fg='#2aaaff'})
+vim.api.nvim_set_hl(0, 'TreesitterContext', {bg='#292929'})
 
