1e58f6e5f06b65d684891da6be82b852878a6ace
Grace Petryk <gracepetryk99@gmail.com>
2023-03-10 00:02:14 -0700

    local after

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -3,9 +3,7 @@ require('rose-pine').setup({
   disable_italics = false,
   highlight_groups = {
     FloatBorder = { fg = 'muted', bg = 'base' },
-    -- TelescopeNormal = { fg='subtle', bg = 'base' },
     TelescopeBorder = { fg = 'muted', bg = 'base' },
-    -- TelescopePromptNormal = { bg = '#191724' },
     LspSignatureActiveParameter = { fg = 'gold' },
     TreeSitterContext = { bg = 'foam', blend = 5 },
     Type = { fg = 'rose' },
--- nvim/init.lua
+++ nvim/init.lua
@@ -12,5 +12,5 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("local")
 require("gpetryk")
+require("local")
