2b74e572017e0ea71fdafe8dd86c2b8930e22f33
Grace Petryk <gpetryk@discoveryed.com>
2023-10-06 09:57:40 -0700

    python sql injections + stuff idk
    Former-commit-id: f8ebe02bc650b3f77040ff3b4b72525f2adae74d

--- nvim/.config/nvim/after/queries/python/injections.scm
+++ nvim/.config/nvim/after/queries/python/injections.scm
@@ -1,4 +1,15 @@
 ; extends
-((string_content) @injection.content
-  (#match? @injection.content "^( |\n)*(SELECT|INSERT|UPDATE|DELETE|CREATE (TABLE|VIEW))( |\n)")
-  (#set! injection.language "sql"))
+(string
+   (string_content) @start @injection.content
+   (#match? @start "^( |\n)*(SELECT|INSERT|UPDATE|DELETE|(CREATE|DROP) (TABLE|VIEW))( |\n)")
+   (#set! injection.language "sql"))
+
+(string
+   (string_start)
+   .
+   (string_content) @start
+   .
+   (interpolation)+
+   (string_content) @rest @injection.content
+   (#match? @start "^( |\n)*(SELECT|INSERT|UPDATE|DELETE|(CREATE|DROP) (TABLE|VIEW))( |\n)")
+   (#set! injection.language "sql"))
