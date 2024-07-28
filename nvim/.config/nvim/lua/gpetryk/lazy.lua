ad97ab674f2aae57354848e1307c7ed2272f97ef
Grace Petryk <gpetryk@discoveryed.com>
2024-07-28 13:05:35 -0700

    misc

--- nvim/.config/nvim/lua/gpetryk/lazy.lua
+++ nvim/.config/nvim/lua/gpetryk/lazy.lua
@@ -16,7 +16,9 @@ return {
   {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
+    config = function ()
+      require('nvim-autopairs').setup()
+    end
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
   },
